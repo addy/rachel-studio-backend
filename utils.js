051f30ef42bb513f58340e4a579cbcd4542ae6da
x4nt <addisonshaw93@gmail.com>
2019-09-07 15:31:04 -0500

    Once more with feeling

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 module.exports.checkEnvironment = envVars => {
-  Object.entries(envVars).forEach((key, envVar) => {
-    if (!envVar) throw new Error(`Missing ${key} environment variable`);
+  Object.entries(envVars).forEach(([key, value]) => {
+    if (!value) throw new Error(`Missing ${key} environment variable`);
   });
 };
