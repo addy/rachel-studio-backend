60d7eebe8db094bcfebadfe8890d648311e4f9ae
x4nt <addisonshaw93@gmail.com>
2019-09-07 15:30:00 -0500

    Object syntax

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 module.exports.checkEnvironment = envVars => {
-  envVars.forEach((key, envVar) => {
+  Object.entries(envVars).forEach((key, envVar) => {
     if (!envVar) throw new Error(`Missing ${key} environment variable`);
   });
 };
