d0a1271bcbb05c7bd97bbba5999eed055a1c21ff
x4nt <addisonshaw93@gmail.com>
2019-09-08 01:52:43 -0500

    Getting everything finished

--- middleware.js
+++ middleware.js
@@ -1,10 +1,14 @@
-let jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 module.exports.checkBearerToken = (req, res, next) => {
   let { authorization } = req.headers;
 
+  authorization = authorization.trim();
   if (authorization) {
-    if (authorization.startsWith('Bearer ')) authorization = authorization.slice(7, authorization);
+    if (authorization.startsWith('Bearer ')) {
+      authorization = authorization.slice(7, authorization.length);
+    }
+
     return jwt.verify(authorization, process.env.AUTH_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).send({ message: 'Unauthorized' });
