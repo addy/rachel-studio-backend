f5652085b669ab3949f1aca7569c21f7d79b7ec1
x4nt <addisonshaw93@gmail.com>
2019-09-15 02:26:29 -0500

    users -> customers

--- app.js
+++ app.js
@@ -3,20 +3,11 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const redis = require('redis');
 const log4js = require('log4js');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const uuidv4 = require('uuid/v4');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
 const sanitizeHtml = require('sanitize-html');
 const { Client } = require('base-api-io');
-const RedisStore = require('connect-redis')(session);
 const { checkEnvironment } = require('./utils');
-const { checkBearerToken } = require('./middleware');
 
 // ENV config variables
 const port = process.env.PORT || 5000;
@@ -25,103 +16,29 @@ const fromEmail = process.env.FROM_EMAIL || 'admin@rachelshawstudio.com';
 const userEmail = process.env.USER_EMAIL || null;
 const baseToken = process.env.BASE_ACCESS_TOKEN || null;
 const stripeToken = process.env.STRIPE_ACCESS_TOKEN || null;
-const authSecret = process.env.AUTH_SECRET || null;
-const sessionSecret = process.env.SESSION_SECRET || null;
 
 // Check all required environment variables
 checkEnvironment({
   USER_EMAIL: userEmail,
   BASE_ACCESS_TOKEN: baseToken,
-  STRIPE_ACCESS_TOKEN: stripeToken,
-  AUTH_SECRET: authSecret,
-  SESSION_SECRET: sessionSecret
+  STRIPE_ACCESS_TOKEN: stripeToken
 });
 
 // Build out clients
 const app = express();
-const redisClient = redis.createClient();
 const baseClient = new Client(baseToken);
 const stripe = require('stripe')(stripeToken);
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(cookieParser());
 app.use(
   cors({
     origin: 'https://rachelshawstudio.com',
     optionsSuccessStatus: 200
   })
 );
-app.use(
-  session({
-    store: new RedisStore({ client: redisClient }),
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-    ttl: 60 * 60
-  })
-);
-
-// User retrieval helper functions
-const findUserByToken = (token, callback) => {
-  redisClient.hgetall(token, (err, user) => {
-    if (err) return callback(err);
-
-    if (!user) {
-      const error = new Error('Incorrect email or password');
-      error.name = 'IncorrectCredentialsError';
-
-      return callback(error);
-    }
-
-    return callback(null, user);
-  });
-};
-
-const findUserByEmail = (email, callback) => {
-  redisClient.hgetall(email, (err, user) => {
-    if (err) return callback(err);
-
-    if (!user) {
-      const error = new Error('Incorrect email or password');
-      error.name = 'IncorrectCredentialsError';
-
-      return callback(error);
-    }
-
-    return callback(null, user);
-  });
-};
-
-// Set up passport authentication
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password'
-    },
-    (email, password, done) => {
-      findUserByEmail(email, (err, user) => {
-        if (err) done(err);
-        else if (!user) done(null, false);
-        else {
-          bcrypt.compare(password, user.password, (bErr, valid) => {
-            if (bErr) done(bErr);
-            else if (!valid) done(null, false);
-            else done(null, user);
-          });
-        }
-      });
-    }
-  )
-);
-
-passport.serializeUser((user, cb) => cb(null, user.token));
-passport.deserializeUser((token, cb) => findUserByToken(token, cb));
-
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 // Logging
 log4js.configure({
@@ -151,13 +68,9 @@ app.use(
 );
 
 // Routes
-app.get('/api/user', checkBearerToken, (req, res) => {
-  const { decoded } = req;
-  res.status(200).send({ user: decoded });
-});
-
-app.post('/api/contact', (req, res) => {
+app.post('/api/contact', async (req, res) => {
   const { firstName, lastName, email, message } = req.body;
+
   if (!firstName || !lastName || !email || !message)
     res.status(500).send({ message: 'Missing required parameters to send email' });
 
@@ -176,60 +89,35 @@ app.post('/api/contact', (req, res) => {
   // And our trusty email title.
   const title = `rachelshawstudio.com - Contact from (${lastName}, ${firstName} <${email}>)`;
 
-  // Unfortunately, the from parameter will not work unless it has the same domain as the mail server.
-  // Best way for now is to override with a bogus email (of the same domain) and make a note in the message body
-  // who the email was really from.
-  baseClient.emails
-    .send(title, fromEmail, userEmail, htmlText, basicText)
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch(() => {
-      res.status(500).send({ message: 'Failed to send email' });
+  try {
+    // Unfortunately, the from parameter will not work unless it has the same domain as the mail server.
+    // Best way for now is to override with a bogus email (of the same domain) and make a note in the message body
+    // who the email was really from.
+    await baseClient.emails.send(title, fromEmail, userEmail, htmlText, basicText);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: 'Failed to send email' });
+  }
+});
+
+app.post('/api/charge', async (req, res) => {
+  try {
+    const { token, email } = req.body;
+
+    const { status } = await stripe.charges.create({
+      amount: 2000,
+      currency: 'usd',
+      description: 'An example charge',
+      source: token,
+      receipt_email: email
     });
-});
 
-app.post('/api/user', (req, res) => {
-  const { email, password } = req.body;
-  redisClient.exists(email, (err, reply) => {
-    if (err) {
-      res.status(500).send({ message: 'Could not check Redis.' });
-    } else if (reply) {
-      res.status(409).send({ message: 'User already exists.' });
-    } else {
-      bcrypt.genSalt(10, (saltErr, salt) => {
-        if (saltErr) {
-          res.status(500).send({ message: 'Could not salt password.' });
-        } else {
-          bcrypt.hash(password, salt, (hashErr, hash) => {
-            if (hashErr) {
-              res.status(500).send({ message: 'Could not hash password.' });
-            } else {
-              const id = uuidv4();
-              // Bearer token is a signature of User ID & User Email.
-              const token = jwt.sign({ id, email }, authSecret);
-              const user = {
-                id,
-                email,
-                token,
-                password: hash
-              };
-
-              redisClient.hmset(email, user);
-              redisClient.hmset(token, user);
-
-              res.status(200).send({ token });
-            }
-          });
-        }
-      });
-    }
-  });
-});
-
-app.post('/api/login', passport.authenticate('local'), (req, res) => {
-  const { token } = req.user;
-  res.status(200).send({ token });
+    res.json({ status });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: 'Failed to charge card' });
+  }
 });
 
 app.listen(port, () => logger.info(`Listening on port ${port}`));
