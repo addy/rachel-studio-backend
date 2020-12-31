bb6515eeb0e67bb89cc6c290b8aa64892d35ae0e
Addison Shaw <addy@addy.sh>
2020-12-31 01:26:17 -0600

    Refactoring into a light weight app file, removing unused dependencies

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-/* eslint-disable import/order */
-/* eslint-disable no-console */
-require('dotenv').config();
-const express = require('express');
-const nodemailer = require("nodemailer");
-const cookieParser = require('cookie-parser');
-const log4js = require('log4js');
-const cors = require('cors');
-const sanitizeHtml = require('sanitize-html');
-const { MongoClient, ObjectId } = require('mongodb');
-const { checkEnvironment } = require('./utils');
+import dotenv from 'dotenv';
+import express from 'express';
+import log4js from 'log4js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import Stripe from 'stripe';
+import Mongo from './mongo';
+import Transporter from './transporter';
+import { checkEnvironment, createEmail } from './utils';
+
+// Configure dotenv, will be mostly dev going forward.
+dotenv.config();
 
 // ENV config variables
 const port = process.env.PORT || 5000;
@@ -32,47 +33,36 @@ checkEnvironment({
   SMTP_PASSWORD: smtpPassword,
   TO_EMAIL: toEmail,
   MONGODB_USER: mongoUser,
-  MONGODB_PASSWORD: mongoPassword
+  MONGODB_PASSWORD: mongoPassword,
+});
+
+// Logging
+log4js.configure({
+  appenders: {
+    file: { type: 'file', filename: logDir },
+  },
+  categories: {
+    default: { appenders: ['file'], level: 'info' },
+  },
 });
 
 // Build out clients
 const app = express();
-const stripe = require('stripe')(stripeToken);
-const transporter = nodemailer.createTransport({
-  host: smtpHost,
-  port: smtpPort,
-  secure: true,
-  auth: {
-    user: smtpUser,
-    pass: smtpPassword,
-  },
-});
-
-let db;
+const logger = log4js.getLogger('app');
+const mongo = new Mongo(mongoHost, mongoPort, mongoUser, mongoPassword);
+const transporter = new Transporter(smtpHost, smtpPort, smtpUser, smtpPassword);
+const stripe = Stripe(stripeToken);
 
 // Middleware
 app.use(express.json());
 app.use(
   cors({
     origin: 'https://rachelshawstudio.com',
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
   })
 );
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-// Logging
-log4js.configure({
-  appenders: {
-    file: { type: 'file', filename: logDir }
-  },
-  categories: {
-    default: { appenders: ['file'], level: 'info' }
-  }
-});
-
-const logger = log4js.getLogger('default');
 app.use(
   log4js.connectLogger(logger, {
     level: 'info',
@@ -85,18 +75,14 @@ app.use(
         )} - :status :content-length ":referrer" ":user-agent"${
           req.method === 'POST' || req.method === 'PUT' ? ` ${JSON.stringify(req.body)}` : ''
         }`
-      )
+      ),
   })
 );
 
 // Site routes
 app.get('/api/art', async (req, res) => {
   try {
-    const documents = await db
-      .collection('art')
-      .find({})
-      .toArray();
-
+    const documents = await mongo.findAllDocuments();
     res.status(200).send(documents);
   } catch (err) {
     logger.error(err);
@@ -107,7 +93,7 @@ app.get('/api/art', async (req, res) => {
 app.get('/api/art/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const document = await db.collection('art').findOne({ _id: ObjectId(id) });
+    const document = await mongo.findDocument(id);
     res.status(200).send(document);
   } catch (err) {
     logger.error(err);
@@ -118,36 +104,13 @@ app.get('/api/art/:id', async (req, res) => {
 app.post('/api/contact', async (req, res) => {
   const { firstName, lastName, email, message } = req.body;
 
-  if (!firstName || !lastName || !email || !message)
+  if (!firstName || !lastName || !email || !message) {
     res.status(500).send({ message: 'Missing required parameters to send email' });
-
-  // Cut out any HTML tags that users inserted into the message.
-  const sanitizedText = sanitizeHtml(message, {
-    allowedTags: [],
-    allowedAttributes: {}
-  });
-
-  // And our trusty email subject.
-  const subject = `rachelshawstudio.com - Contact from (${lastName}, ${firstName} <${email}>)`;
-
-  // This is what $TO_EMAIL will see if their email client has HTML disabled.
-  const text = `Rachel Shaw Studio - Contact Form\nMessage:\n${sanitizedText}`;
-
-  // This is the HTML version of the above.
-  const html = `<h1>Rachel Shaw Studio - Contact Form</h1><p>${sanitizedText}</p><a href="rachelshawstudio.com">rachelshawstudio.com</a>`;
+  }
 
   try {
-    // Nodemailer is deprecating their rate limiter API, so I will do single connections until I can build out the rate limiter myself.
-    const message = {
-      from: smtpUser,
-      replyTo: email,
-      to: toEmail,
-      subject,
-      text,
-      html,
-    };
-    
-    await transporter.sendMail(message);
+    const transportMessage = createEmail(firstName, lastName, email, message);
+    await transporter.sendEmail(email, transportMessage);
     res.sendStatus(200);
   } catch (err) {
     logger.error(err);
@@ -160,30 +123,28 @@ app.post('/api/charge', async (req, res) => {
   const { token, email, artID, price } = req.body;
 
   try {
-    const doc = await db.collection('art').findOne({ _id: ObjectId(artID) });
+    const document = await mongo.findDocument(artID);
 
+    // TODO: Add an 'amount left' integer to each art document
     // First check if we have already sold this piece
-    if (doc.sold) {
+    if (document.sold) {
       res.status(400).send({ message: 'Already sold' });
     } else {
       // Process the payment through Stripe.
       await stripe.charges.create({
-        amount: price ? price * 100 : doc.price * 100,
+        amount: price ? price * 100 : document.price * 100,
         currency: 'usd',
-        description: `${doc.title}${price !== doc.price ? ' print' : ''} for ${email}`,
+        description: `${document.title}${price !== document.price ? ' print' : ''} for ${email}`,
         source: token,
-        receipt_email: email
+        receipt_email: email,
       });
 
-      // Not selling a print. I hate this.
-      if (price === doc.price) {
+      // TODO: Add a type to each art document
+      if (price === document.price) {
         // Mark the art document as sold.
-        await db.collection('art').updateOne(
-          { _id: ObjectId(artID) },
-          {
-            $set: { sold: true }
-          }
-        );
+        await mongo.updateDocument(artID, {
+          $set: { sold: true },
+        });
       }
 
       // Respond with the current ID so that the UI can self-update.
@@ -195,17 +156,4 @@ app.post('/api/charge', async (req, res) => {
   }
 });
 
-app.listen(port, async () => {
-  try {
-    const client = await MongoClient.connect(`mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`, { useUnifiedTopology: true });
-    db = client.db('art');
-
-    // Validate the transport once
-    await transporter.verify()
-  } catch (err) {
-    logger.error(err);
-    throw new Error(`Could not connect to MongoDB`);
-  }
-
-  logger.info(`Listening on port ${port}`);
-});
+app.listen(port, () => logger.info(`Listening on port ${port}`));
--- utils.js
+++ utils.js
@@ -1,6 +1,25 @@
-/* eslint-disable import/prefer-default-export */
-module.exports.checkEnvironment = envVars => {
+import sanitizeHtml from 'sanitize-html';
+
+export function checkEnvironment(envVars) {
   Object.entries(envVars).forEach(([key, value]) => {
     if (!value) throw new Error(`Missing ${key} environment variable`);
   });
-};
+}
+
+export function createEmail(firstName, lastName, email, message) {
+  // Cut out any HTML tags that users inserted into the message.
+  const sanitizedText = sanitizeHtml(message, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
+
+  const subject = `rachelshawstudio.com - Contact from (${lastName}, ${firstName} <${email}>)`;
+  const text = `Rachel Shaw Studio - Contact Form\nMessage:\n${sanitizedText}`;
+  const html = `<h1>Rachel Shaw Studio - Contact Form</h1><p>${sanitizedText}</p><a href="rachelshawstudio.com">rachelshawstudio.com</a>`;
+
+  return {
+    subject,
+    text,
+    html,
+  };
+}
